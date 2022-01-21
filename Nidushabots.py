ecc9c0e42b9ace9fcd650f203f23ca3c68fc0d30
NidushaAmarasinghe <95688464+NidushaAmarasinghe@users.noreply.github.com>
2022-01-21 08:49:33 -0800

    Update Nidushabots.py

--- Nidushabots.py
+++ Nidushabots.py
@@ -18,85 +18,32 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+from cgitb import text
 import telebot, requests, json
 from telebot import types
 from os import getenv
 
 bot = telebot.TeleBot(getenv("BOT_TOKEN"))
 
-# HPB API
-response_API = requests.get('https://hpb.health.gov.lk/api/get-current-statistical')
-data = json.loads(response_API.text)
-local_new_cases     = str(data['data']['local_new_cases'])
-update_date_time    = str(data['data']['update_date_time'])
-local_new_cases     = str(data['data']['local_new_cases'])
-local_active_cases  = str(data['data']['local_active_cases'])
-local_total_cases   = str(data['data']['local_total_cases'])
-local_deaths        = str(data['data']['local_deaths'])
-local_recovered     = str(data['data']['local_recovered'])
-local_total_number_of_individuals_in_hospitals = str(data['data']['local_total_number_of_individuals_in_hospitals'])
-global_new_cases    = str(data['data']['global_new_cases'])
-global_total_cases  = str(data['data']['global_total_cases'])
-local_new_deaths    = str(data['data']['local_new_deaths'])
-global_deaths       = str(data['data']['global_deaths'])
-global_new_deaths   = str(data['data']['global_deaths'])
-global_recovered    = str(data['data']['global_recovered'])
-
-# /covid command menu
-covidinfo = f"""
-ශ්‍රී ලංකාවේ කොරෝනා තත්වය. 🇱🇰
-🔄 {update_date_time} ට යාවත්කාලීන කරන ලදී.
-• නව රෝගීන් ගණන 😷 - {local_new_cases}
-• නව මරණ ගණන ⚰ - {local_new_deaths}
-• තහවුරු කරන ලද මුළු රෝගීන් ගණන 🤒 - {local_total_cases}
-• තවමත් ප්‍රතිකාර ලබන රෝගීන් ගණන 🤕 - {local_active_cases}
-• මේ වන විට සුව වූ කොරෝන රෝගීන් ගණන 🙂 - {local_recovered}
-• මුළු මරණ සංඛ්‍යාව ⚰ - {local_deaths}
-Join @SlapTap
-"""
-
-# /gcovid command menu
-gcovidinfo = f"""
-සමස්ත ලෝකයේ කොරෝනා තත්වය. 🌎
-🔄{update_date_time} ට යාවත්කාලීන කරන ලදී.
-• නව රෝගීන් ගණන 😷 - {global_new_cases}
-• නව මරණ ගණන ⚰ - {global_new_deaths}
-• තහවුරු කරන ලද මුළු රෝගීන් ගණන 🤒 - {global_total_cases}
-• මේ වන විට සුව වූ කොරෝන රෝගීන් ගණන 🙂 - {global_recovered}
-• මුළු මරණ සංඛ්‍යාව ⚰ - {global_deaths}
-Join @SlapTap
-"""
-# /help command menu
-help = f"""
-භාවිතා කළ හැකි විධාන 📌
- • /start - BOT start කිරීම සදහා.
- • /covid - ශ්‍රී ලංකාවේ නවතම කොරෝනා තොරතුරු සදහා.
- • /gcovid -සමස්ත ලෝකයේ නවතම කොරෝනා තොරතුරු සදහා.
- • /about - BOT ගැන දැන ගැනීම සදහා.
-Inline mode 
- • Inline mode එකේ covid කියලා type කරන්න.
- Join @SlapTap
-"""
 
 # Markup
 mark1 = telebot.types.InlineKeyboardMarkup()
-mark1.add(telebot.types.InlineKeyboardButton(text='Updates', url='https://t.me/SlapTap'),
-          telebot.types.InlineKeyboardButton(text='Support', url='https://t.me/SlapTaps')),
-mark1.add(telebot.types.InlineKeyboardButton(text='Latest Details', callback_data=1)),
-mark1.add(telebot.types.InlineKeyboardButton(text='Go Inline', switch_inline_query_current_chat="")),
+mark1.add(telebot.types.InlineKeyboardButton(text='🔁Updates🔁', url='https://t.me/SlapTap'),
+          telebot.types.InlineKeyboardButton(text='🧑‍💻Support🧑‍💻', url='https://t.me/SlapTaps')),
+mark1.add(telebot.types.InlineKeyboardButton(text='🛠️Socure🛠️', url='https://github.com/NidushaAmarasinghe')),
 
 mark2 = telebot.types.InlineKeyboardMarkup()
-mark2.add(telebot.types.InlineKeyboardButton(text='Latest Details', callback_data=1),
-          telebot.types.InlineKeyboardButton(text='Go Inline', switch_inline_query_current_chat=""))
+mark2.add(telebot.types.InlineKeyboardButton(text='🛠️Socure🛠️', url='https://github.com/NidushaAmarasinghe'),
+          
 
 # Commands
 @bot.message_handler(commands=['start'])
 def send_start(message):
-   bot.send_message(message.chat.id, text="Hi, කොරෝනා පිළිබද තොරතුරු ලබා ගැනීම සදහා මාව භාවිතා කරන්න පුළුවන්. භාවිතා කරන ආකාරය දැන ගැනීමට /help භාවිතා කරන්න.",parse_mode='Markdown', reply_markup=mark1)
+   bot.send_message(message.chat.id, text="💕Hi There! 😁Welcome To Nidusha Official Bot😘\nJoin @SlapTap",parse_mode='Markdown', reply_markup=mark1)
 
-@bot.message_handler(commands=["covid"])
+@bot.message_handler(commands=["bots"])
 def send_covid(message):
-    bot.send_message(message.chat.id, covidinfo)
+    bot.send_message(message.chat.id, text="Go To @Nidusha_Bot")
 
 @bot.message_handler(commands=["gcovid"])
 def send_gcovid(message):
@@ -109,8 +56,8 @@ def send_help(message):
 @bot.message_handler(commands=["about"])
 def send_about(message):
     bot.send_message(message.chat.id, """
-• මෙම BOT කොරෝනා තොරතුරු දැන ගැනීම සදහා Nidusha Amarasinghe විසින් සාදන ලද Bot කෙනෙකි.
-• සියලු තොරතුරු [සෞඛ්‍ය ප්‍රවර්ධන කාර්‍යංශයෙන්](https://hpb.health.gov.lk) ලබා ගත් තොරතුරු ය.
+• Bot Deverloper-{@NidushaAmarasinghe}
+• This Is Nidusha Official Bot
                                       """, parse_mode='Markdown')
 
 # Callback Data
@@ -122,10 +69,10 @@ def query_handler(call):
     bot.send_message(call.message.chat.id, answer)           
 
 # Inline Mode             
-@bot.inline_handler(lambda query: query.query == 'covid')
+@bot.inline_handler(lambda query: query.query == 'bots')
 def query_text(inline_query):
-        in1 = types.InlineQueryResultArticle('1', "ශ්‍රී ලංකාවේ කොරෝනා තත්වය. 🇱🇰", types.InputTextMessageContent(covidinfo))
-        in2 = types.InlineQueryResultArticle('2', "සමස්ත ලෝකයේ කොරෝනා තත්වය. 🌎", types.InputTextMessageContent(gcovidinfo))
-        bot.answer_inline_query(inline_query.id, [in1, in2])
+        in1 = types.InlineQueryResultArticle('1', "My Bots", types.InputTextMessageContent(Click Here To Get Bot List /bots))
+        
+        bot.answer_inline_query(inline_query.id, [in1])
     
 bot.polling()
